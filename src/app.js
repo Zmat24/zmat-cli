1ae160c21edca17af870a5a8b0377a0f1d8547ae
Matin Soleymani <matinsoleymni@gmail.com>
2023-08-13 10:55:21 +0330

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,49 @@
+import arg from 'arg';
+const { exec } = require("child_process");
+function parse(rawArgs) {
+const args = arg(
+{
+    "--create-site": Boolean,
+},
+    {
+        argv: rawArgs.slice(2),
+    }
+);
+return {
+    site: args['--create-site'] || false,
+};
+}
+
 export function cli(args) {
-    console.log(args);
+    let options = parse(args);
+    if(options.site){
+        exec("mkdir js css images fonts", (error, stdout) => {
+            if (error) {
+                console.log(`Zmat-cli:: error: ${error.message}`);
+                return;
+            }
+            console.log(`Zmat-cli: ${stdout || "created"}`);
+        });
+        exec("echo /* created by zmat-cli */ > css/style.css ", (error, stdout) => {
+            if (error) {
+                console.log(`Zmat-cli:: error: ${error.message}`);
+                return;
+            }
+            console.log(`Zmat-cli: ${stdout || "created"}`);
+        });
+        exec("echo /* created by zmat-cli */ > js/script.js ", (error, stdout) => {
+            if (error) {
+                console.log(`Zmat-cli:: error: ${error.message}`);
+                return;
+            }
+            console.log(`Zmat-cli: ${stdout || "created"}`);
+        });
+        exec(`echo ^<!-- Created By Zmat-cli --^> > index.html `, (error, stdout) => {
+            if (error) {
+                console.log(`Zmat-cli:: error: ${error.message}`);
+                return;
+            }
+            console.log(`Zmat-cli: ${stdout || "created"}`);
+        });
+    }
 }
