811fc322f24f50b19ca9120048b43bc19cb12372
Matin Soleymani <110892827+matinsoleymni@users.noreply.github.com>
2023-08-14 00:30:00 +0330

    Update exports.js
    Signed-off-by: Matin Soleymani <110892827+matinsoleymni@users.noreply.github.com>

--- src/exports.js
+++ src/exports.js
@@ -23,4 +23,11 @@ const css =
 `/* created by zmat-cli */
 body{background-color:#3b3b3b;display:flex;justify-content:center;align-items:center;height:400px}h1{font-size:50px;color:#fff;text-shadow:5px 5px 5px #000;animation:2s infinite alternate zmat}@keyframes zmat{0%{transform:translateY(50px)}100%{transform:translateY(100px)}}`
 
-module.exports = {html , css} ;
+const help =` Zmat-cli: help
+
+        -site : --create-site    : => make new project
+        -v    : --version "      : => show Version
+        -h    : --help "         : => show Help
+
+        `)}}
+module.exports = {html , css , help} ;
