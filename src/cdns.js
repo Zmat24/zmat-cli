c01070d643202a8b3c551196966f8d8c1329c02b
zeynab <zeynab.khayyati.11@gmail.com>
2023-08-17 11:26:37 +0330

    minify

--- src/cdns.js
+++ src/cdns.js
@@ -1,6 +1,1 @@
-const cdn = {
-    tailwind: "https://cdn.tailwindcss.com",
-    css24    : "https://blokchainology.com/CDN/min24.css"
-}
-
-module.exports = cdn
+const cdn={tailwind:"https://cdn.tailwindcss.com",css24:"https://blokchainology.com/CDN/min24.css"};module.exports=cdn;
